fca7e96c4593a623083f8fb57ae044a0b3f298f9
mbozik <bozik.kizob@gmail.com>
2021-04-25 19:35:17 +0200

    test

--- api/index.php
+++ api/index.php
@@ -7,7 +7,7 @@ session_start();
 <html lang="en">
 <head>
 <meta charset="UTF-8">
-<link rel="stylesheet" href="api/css/style.css">
+<link rel="stylesheet" href="css/style.css">
 <meta name="viewport" content="width=device-width, initial-scale=1.0"> <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>login tutorial</title>
 </head> 
