1284bb1db3d9f8926282c8ddbbc8b0f8c05fc44c
mbozik <bozik.kizob@gmail.com>
2021-04-27 17:22:20 +0200

    test

--- api/index.php
+++ api/index.php
@@ -24,7 +24,7 @@ session_start();
     <header>
     <div class="menu">
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
-          <a class="navbar-brand" href="index.html">Projekt inżynierski</a>
+          <a class="navbar-brand" href="index.php">Ankiety</a>
 
           <button class="navbar-toggler collapsed" type="button" data-toggle="collapse"
                   data-target="#navbarSupportedContent"
