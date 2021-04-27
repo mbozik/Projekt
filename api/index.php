7b22b9f4f2a5db9dc5dc0c7f6c726b64e8865be9
mbozik <bozik.kizob@gmail.com>
2021-04-27 16:23:47 +0200

    log

--- api/index.php
+++ api/index.php
@@ -9,7 +9,7 @@ session_start();
 <meta charset="UTF-8">
 <link rel="stylesheet" href="css/style.css">
 <meta name="viewport" content="width=device-width, initial-scale=1.0"> <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>login tutorial</title>
+  <title>Login</title>
 </head> 
 <body>
     <header>
