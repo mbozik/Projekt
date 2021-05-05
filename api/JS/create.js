b76e15a1d446f90d046d72e507285ad7231a2dea
mbozik <bozik.kizob@gmail.com>
2021-05-05 15:14:03 +0200

    test

--- api/JS/create.js
+++ api/JS/create.js
@@ -1,46 +1,160 @@
-    $(document).ready(function(){
-    var i=0;
-    var numOfQuestions=0;
-    var choice_c = 1;
-    $("#addq").click(function(){
-            numOfQuestions++;
-            i++;
-            $("#questions").append("<div id='newq' style='margin-top: 5px; margin-bottom: 5px'></div>");
-            $("#newq").append("Question: <input type='text' name='q"+i+"' class='form-control' style='display: inline-block' id='q"+i+"'/>");
-            $("#qnum").attr("value", numOfQuestions);
-            $("#newq").append("<label><input type='radio' name='radio"+i+"' value='radio' class='choices radiobox' id='radiobox"+i+"'/>&nbsp;Radiobox&nbsp;&nbsp;</label>");
-            $("#newq").append("<label><input type='radio' name='radio"+i+"' value='check' class='choices checkbox' id='checkbox"+i+"'/>&nbsp;Checkbox&nbsp;&nbsp;</label>");
-            $("#newq").append("<label><input type='radio' name='radio"+i+"' value='text' class='choices textbox' id='textbox"+i+"'/>&nbsp;Text&nbsp;&nbsp;</label>");
-            $("#newq").append("<input type='text' class='form-control qchoice' name='radiochoice"+choice_c+"_q"+i+"' id='radiochoice"+choice_c+"' title='q"+i+"' style='display: none'/>");
-            choice_c++;
-            $("#newq").append("<input type='text' class='form-control qchoice' name='radiochoice"+choice_c+"_q"+i+"' id='radiochoice"+choice_c+"' title='q"+i+"' style='display: none'/>");
-            choice_c++;
-            $("#newq").append("<button id='radiobtn"+i+"' type='button' class='btn btn-primary add-radio-choice' style='display: none'><span class='glyphicon glyphicon-plus'></span>Dodatkowa odpowiedź</button>");
-            $("#newq").append("<button id='checkbtn"+i+"' type='button' class='btn btn-primary add-checkbox-choice' style='display: none'><span class='glyphicon glyphicon-plus'></span>Dodatkowa odpowiedź</button>");
+$(document).ready(function() {
+    var i = 0;
+    var numOfQuestions = 0;
+    var labelEl = $("<label>");
+    var inputEl = $("<input>");
+    // Handle the user clicking on "Add a question"
+    $("#addq").click(function() {
+      // Increment both counters,
+      numOfQuestions++;
+      i++;
+      // a nested function creates the HTML DOM structure.
+      addQuestion();
+      // Handle the user choosing what type of question they want
+      $(".choices").on("change", function() {
+        var option = $(this).val();
+        switch (option) {
+          case "radio":
+            showRadioOpts();
+            break;
+          case "checkbox":
+            showCheckboxOpts();
+            break;
+          case "text":
+            showTextOpts();
+            break;
+        }
+      })
+  
+    });
+  
+    /**
+     * This handles the HTML DOM creation. I don't want to clog up
+     *   the main routine with all the ugly, so I've moved it here.
+     *   Purely cosmetic. The functioning is the same as the former
+     *   append() functions with the element completely spelled out.
+     **/
+    function addQuestion() {
+  
+      var newQuestionEl = inputEl.clone().prop({
+        "type": "text",
+        "name": "q" + i,
+        "id": "q" + i,
+        "class": "form-control"
+      });
+      var newQuestion = labelEl.clone().prop({
+        "for": "q" + i,
+        "class": "form-control"
+      }).append("Question: ", newQuestionEl);
+  
+      var newQTypeArr = [];
+      var newQTypeRadioEl = inputEl.clone().prop({
+        type: "radio",
+        name: "qType" + i,
+        id: "qType" + i,
+        value: "radio",
+        class: "choices radiobox"
+      });
+      newQTypeArr[0] = labelEl.clone().append(newQTypeRadioEl, " Radio");
+  
+      var newQTypeCheckEl = inputEl.clone().prop({
+        type: "radio",
+        name: "qType" + i,
+        id: "qType" + i,
+        value: "checkbox",
+        class: "choices radiobox"
+      });
+      newQTypeArr[1] = labelEl.clone().append(newQTypeCheckEl, "Checkbox");
+      var newQTypeTextEl = inputEl.clone().prop({
+        type: "radio",
+        name: "qType" + i,
+        id: "qType" + i,
+        value: "text",
+        class: "choices radiobox"
+      });
+      newQTypeArr[2] = labelEl.clone().append(newQTypeTextEl, "Text");
+      var answerOptionsEl = $("<div>").prop({
+        class: "answer-options-pane"
+      });
+      var newAnsContainerEl = $("<div>").prop({
+        class: "answer-pane"
+      }).append(newQTypeArr, answerOptionsEl);
+      var newQContainerEl = $("<div>").prop({
+        id: "newq"
+      }).append(newQuestion, newAnsContainerEl);
+  
+      $("#questions").append(newQContainerEl);
+      $("#qnum").attr("value", numOfQuestions);
+    }; //end addQuestion()
+  
+    // Toggle the radio answer options
+    function showRadioOpts() {
+      // First, hide all answer options. Then, add one radio option.
+      $(".answer-options-pane").empty();
+      addRadioOpts();
+  
+    }; // end showRadioOpts()
+  
+    // Toggle the checkbox answer options
+    function showCheckboxOpts() {
+      $(".answer-options-pane").empty();
+    }; // end showCheckboxOpts() 
+  
+    // Toggle the text box answer options
+    function showTextOpts() {
+      $(".answer-options-pane").empty();
+      addTextOpts();
+    }; // end showTextOpts()
+  
+    /***
+     * Another DOM element creation function. This creates the radio
+     *   button text option, and if it's the first, a button to add
+     *   more options. 
+     ***/
+    function addRadioOpts() {
+      // We want to get the length of the current choices, 
+      //  as this will give us an index for the new option
+      
+      var radioChoice = $(".radio-choice");
+      var choice_c = radioChoice.length;
+      var radioChoiceTextEl = inputEl.clone().prop({
+        "type": "text",
+        "class": "form-control answer-option radio-choice",
+        "name": "radiochoice" + choice_c + "_q" + i,
+        "id": "radiochoice" + choice_c + "_q" + i,
+        "title": "q" + i
+      });
+      // If we don't have any radio elements yet, we will also
+      //  want to create an "add more options" button.
+      if (choice_c <= 0) {
+        var addIconEl = $("<span>").prop({
+          "class": "glyphicon glyphicon-plus"
         });
-        $(document.body).on('change', '.choices' ,function() {
-            if ($(".radiobox").is(":checked")) {
-                $(".add-radio-choice").show();
-                $(".qchoice").show();
-                $(".add-checkbox-choice").hide();
-            }
-            else if ($(".checkbox").is(":checked")) {
-                $(".add-checkbox-choice").show();
-                $(".add-radio-choice").hide();
-                $(".qchoice").show();
-            }
-            else if($(".textbox").is(":checked")){
-                $(".add-checkbox-choice").hide();
-                $(".add-radio-choice").hide();
-                $(".qchoice").hide();
-            }
+        var addChoiceButton = $("<button>").prop({
+          "id": "radiobtn" + i,
+          "class": "btn btn-primary add-radio-choice answer-option"
+        }).append(addIconEl, "Add choices").on("click", function(evt) {
+          // Make sure you don't let that button do what buttons do...
+          evt.preventDefault();
+          addRadioOpts()
         });
-        $(document.body).on('click', '.add-radio-choice' ,function(){
-            $("#newq").append("Choice: <input type='text' name='radiochoice"+choice_c+"_q"+i+"' title='q"+i+"' class='form-control qchoice' id='radiochoice"+choice_c+"'/>");
-            $("#choicenum").attr("value", choice_c);
-        });
-        $(document.body).on('click', '.add-checkbox-choice' ,function(){
-            $("#newq").append("<input type='text' name='radiochoice"+choice_c+"_q"+i+"' title='q"+i+"' class='form-control qchoice' id='checkboxchoice"+choice_c+"'>");
-            $("#choicenum").attr("value", choice_c);
-        });
-});
+        $(".answer-options-pane").append(addChoiceButton);
+      }
+      radioChoiceEl = labelEl.clone().append(radioChoiceTextEl);
+      // Make sure to add the new text element BEFORE the 
+      //    add more button.
+      $(".add-radio-choice").before(radioChoiceEl);
+    };
+    
+    function addTextOpts(){
+      var textChoiceTextEl = inputEl.clone().prop({
+        "type": "text",
+        "class": "form-control answer-option text-choice",
+        "name": "radiochoice_q" + i,
+        "id": "textchoice_q" + i,
+        "title": "q" + i
+      });
+          textChoiceEl = labelEl.clone().append("Text: ", textChoiceTextEl);
+      $(".answer-options-pane").append(textChoiceEl)
+      }
+  });
--- api/create.php
+++ api/create.php
@@ -1,20 +1,145 @@
+<?php
+
+//create.php
+
+session_start();
+
+?>
 <!DOCTYPE html>
-<html lang="en">
-    <head>
-        <title>Use options to customize the Survey Creator, Survey Creator Example</title><meta name="viewport" content="width=device-width"/>
-        <script src="https://unpkg.com/knockout@3.5.1/build/output/knockout-latest.js"></script>
-        <script src="https://unpkg.com/survey-knockout@1.8.46/survey.ko.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/ace/1.4.10/ace.min.js" type="text/javascript" charset="utf-8"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/ace/1.4.10/ext-language_tools.js" type="text/javascript" charset="utf-8"></script>
-        <!-- Uncomment to enable Select2 <script src="https://unpkg.com/jquery"></script> <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/css/select2.min.css" rel="stylesheet" /> <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/select2.min.js"></script> -->
-        <link href="https://unpkg.com/survey-creator@1.8.46/survey-creator.min.css" type="text/css" rel="stylesheet"/>
-        <script src="https://unpkg.com/survey-creator@1.8.46/survey-creator.min.js"></script>
-        <link rel="stylesheet" href="https://unpkg.com/bootstrap@3.3.7/dist/css/bootstrap.min.css">
-        <link rel="stylesheet" href="./index.css"></head>
-    <body>
-        <div id="surveyContainer">
-            <div id="creatorElement"></div>
+<html>
+ <head>
+  <title>Projekt inżynierski ankiety</title>
+
+<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet"/>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<link rel="stylesheet" href="css/style.css">
+<script type="text/javascript"
+        src="//cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.min.js">
+</script>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+          <!--<script src="js/create.js"></script>-->
+<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+  <script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.5.7/angular.min.js"></script>
+<header>
+    <div class="menu">
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <a class="navbar-brand" href="index.php">Ankiety</a>
+
+          <button class="navbar-toggler collapsed" type="button" data-toggle="collapse"
+                  data-target="#navbarSupportedContent"
+                  aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+          </button>
+          <?php
+   if(isset($_SESSION["name"]))
+   {
+   ?>
+
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+           <!--       <li class="nav-item active">
+                      <a id="nav-login" class="nav-link" href="login.php">Logowanie</a>
+                  </li>
+                  <li class="nav-item">
+                      <a id="nav-register" class="nav-link" href="signup.php">Rejestracja</a>
+                  </li> -->
+                 <li class="nav-item">
+                      <a id="logout" class="nav-link logout" href="logout.php">Wyloguj</a>
+                  </li>   
+                 <li class="nav-item">
+                      <a id="profile" class="nav-link" href="profile.php">Profil</a>
+                  </li> <?php }else {
+                      ?> 
+                      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                      <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                          <li class="nav-item active">
+                              <a id="nav-login" class="nav-link" href="index.php">Logowanie</a>
+                          </li>
+                          <li class="nav-item">
+                              <a id="nav-register" class="nav-link" href="index.php">Rejestracja</a>
+                          </li> 
+                      <!--   <li class="nav-item">
+                              <a id="logout" class="nav-link logout" href="logout.php">Wyloguj</a>
+                          </li>   
+                         <li class="nav-item">
+                              <a id="profile" class="nav-link" href="profile.php">Profil</a>-->
+                              <?php   }?> 
+              </ul>
+          </div>
+      </nav>
+  </div>
+    
+<div class="container" style="margin-top: 5%">
+  <div class="row">
+    <div class="col-md-12">
+      <div class="panel panel-default">
+        <div class="panel-heading">Create New Survey:</div>
+        <div class="panel-group">
+          <form  method="POST" class="form-control" style="height: 700px; border: none" id="survey-form">
+            <label>Survey Title: </label>
+            <input name="survey_title" id="survey_title" type="text" class="form-control input-group" />
+            <input name="qnum" id="qnum" type="hidden" value="" />
+            <input name="choicenum" id="choicenum" type="hidden" value="" />
+            <label>Survey Questions:</label>
+            <div id="questions">
+            </div>
+            <button class="btn btn-primary" type="button" style="display: block; margin-top: 5px;" id="addq"><span class="glyphicon glyphicon-plus"></span>Add a question</button>
+            <button class="btn btn-success" type="submit" id="uploadsurvey" name="uploadsurvey" style="display: block; margin-top: 10px">Upload Survey</button>
+          </form>
         </div>
-        <script type="text/javascript" src="index.js"></script>
-    </body>
-</html>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+<script>
+
+    $(document).ready(function(){
+    var i=0;
+    var numOfQuestions=0;
+    var choice_c = 1;
+    $("#addq").click(function(){
+            numOfQuestions++;
+            i++;
+            $("#questions").append("<div id='newq' style='margin-top: 5px; margin-bottom: 5px'></div>");
+            $("#newq").append("Question: <input type='text' name='q"+i+"' class='form-control' style='display: inline-block' id='q"+i+"'/>");
+            $("#qnum").attr("value", numOfQuestions);
+            $("#newq").append("<label><input type='radio' name='radio"+i+"' value='radio' class='choices radiobox' id='radiobox"+i+"'/>&nbsp;Radiobox&nbsp;&nbsp;</label>");
+            $("#newq").append("<label><input type='radio' name='radio"+i+"' value='check' class='choices checkbox' id='checkbox"+i+"'/>&nbsp;Checkbox&nbsp;&nbsp;</label>");
+            $("#newq").append("<label><input type='radio' name='radio"+i+"' value='text' class='choices textbox' id='textbox"+i+"'/>&nbsp;Text&nbsp;&nbsp;</label>");
+            $("#newq").append("<input type='text' class='form-control qchoice' name='radiochoice"+choice_c+"_q"+i+"' id='radiochoice"+choice_c+"' title='q"+i+"' style='display: none'/>");
+            choice_c++;
+            $("#newq").append("<input type='text' class='form-control qchoice' name='radiochoice"+choice_c+"_q"+i+"' id='radiochoice"+choice_c+"' title='q"+i+"' style='display: none'/>");
+            choice_c++;
+            $("#newq").append("<button id='radiobtn"+i+"' type='button' class='btn btn-primary add-radio-choice' style='display: none'><span class='glyphicon glyphicon-plus'></span>Add choices</button>");
+            $("#newq").append("<button id='checkbtn"+i+"' type='button' class='btn btn-primary add-checkbox-choice' style='display: none'><span class='glyphicon glyphicon-plus'></span>Add choices</button>");
+        });
+        $(document.body).on('change', '.choices' ,function() {
+            if ($(".radiobox").is(":checked")) {
+                $(".add-radio-choice").show();
+                $(".qchoice").show();
+                $(".add-checkbox-choice").hide();
+            }
+            else if ($(".checkbox").is(":checked")) {
+                $(".add-checkbox-choice").show();
+                $(".add-radio-choice").hide();
+                $(".qchoice").show();
+            }
+            else if($(".textbox").is(":checked")){
+                $(".add-checkbox-choice").hide();
+                $(".add-radio-choice").hide();
+                $(".qchoice").hide();
+            }
+        });
+        $(document.body).on('click', '.add-radio-choice' ,function(){
+            $("#newq").append("Choice: <input type='text' name='radiochoice"+choice_c+"_q"+i+"' title='q"+i+"' class='form-control qchoice' id='radiochoice"+choice_c+"'/>");
+            $("#choicenum").attr("value", choice_c);
+        });
+        $(document.body).on('click', '.add-checkbox-choice' ,function(){
+            $("#newq").append("<input type='text' name='radiochoice"+choice_c+"_q"+i+"' title='q"+i+"' class='form-control qchoice' id='checkboxchoice"+choice_c+"'>");
+            $("#choicenum").attr("value", choice_c);
+        });
+});
+</script>
