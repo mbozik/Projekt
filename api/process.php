36bf2a5bb4150627d1b87e01bc408c8ae517ab31
benekin <beniamin.bartusiak@gmail.com>
2021-05-19 18:56:11 +0200

    w

--- api/process.php
+++ api/process.php
@@ -1,12 +1,21 @@
 <?php
 
 include('php/db.php');
+// session_start();
 $i = $_POST['i'];
 // $description = $_POST['survey_opis'];
 
 // $sql = "INSERT INTO ankieta(a_id, a_temat, a_opis) VALUES ('','$i','$i')";
-$sql = "INSERT INTO pytania(p_id, pytanie, p_t_id, p_a_id) VALUES ('','$i','1','114')";
-// $id = "SELECT p_id FROM pytania WHERE pytanie="$i"";
-// $zp = "INSERT INTO ankieta(a_id, a_temat, a_opis) VALUES ('','$id','$id')";
+// $sql = "INSERT INTO pytania(p_id, pytanie, p_t_id, p_a_id) VALUES ('','$i','1','114')";
 
-$result = $connect->query($sql);
+// $result = $connect->query($sql);
+
+$query = "SELECT email FROM users";
+$result = $connect->query($query);
+
+while ($row = $result->fetch_assoc()) {
+    $pid = $row["email"];
+}
+
+$zp = "INSERT INTO pytania(p_id, pytanie, p_t_id, p_a_id) VALUES ('','','1','114')";
+$result = $connect->query($zp);
