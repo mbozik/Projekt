daa5c23de384c6fd2b2c65278a082b1901cf582d
benekin <beniamin.bartusiak@gmail.com>
2021-05-20 13:46:44 +0200

    zmiana

--- api/php/insert.php
+++ api/php/insert.php
@@ -1,5 +1,5 @@
 <?php
-// include('db.php');
+include('testowe.php');
 $servername = "localhost";
 $username = "root";
 $password = "";
@@ -7,35 +7,36 @@ $dbname = "baza";
 
 // Create connection
 $connect = new mysqli($servername, $username, $password, $dbname);
+if(($i = $_POST['i'])==false){
+  session_start();
+  $user= $_SESSION['name'];
+  // $a=$tab[0];
+  $title = $_POST['survey_title'];
+  $description = $_POST['survey_opis'];
 
-session_start();
-$user= $_SESSION['name'];
+  $query = "SELECT a_id FROM ankieta";
 
-$title = $_POST['survey_title'];
-$description = $_POST['survey_opis'];
+  $sql = "INSERT INTO ankieta(a_id, a_temat, a_opis, tworca) VALUES ('','$title','$description','$user')";
+  $result = $connect->query($sql);
+  $connect->close();
+}else{
 
-$query = "SELECT a_id FROM ankieta";
+  $sql = "SELECT * from ankieta WHERE a_temat='$title'";
+  $result = $connect->query($sql);
 
-$sql = "INSERT INTO ankieta(a_id, a_temat, a_opis, tworca) VALUES ('','$title','$description','$user')";
-$result = $connect->query($sql);
+  if ($result->num_rows > 0) {
+    // output data of each row
+    $i = $_POST['i'];
 
+    while($row = $result->fetch_assoc()) {
+      $a = $row["a_id"];
+      $zp = "INSERT INTO pytania(p_id, pytanie, p_t_id, p_a_id) VALUES ('','$i','1','$a')";
+      $result = $connect->query($zp);
+    }
+  } 
 
-$sql = "SELECT * from ankieta WHERE a_temat='$title'";
-$result = $connect->query($sql);
-
-if ($result->num_rows > 0) {
-  // output data of each row
-  $i = $_POST['i'];
-
-  while($row = $result->fetch_assoc()) {
-    $a = $row["a_id"];
-    $zp = "INSERT INTO pytania(p_id, pytanie, p_t_id, p_a_id) VALUES ('','$i','1','$a')";
-    $result = $connect->query($zp);
-  }
-} 
-
-$connect->close();
-
+  $connect->close();
+}
 
 
 ?>
