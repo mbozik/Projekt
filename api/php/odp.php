50645bec649380a808634f6d3469258f6374a0dd
mbozik <bozik.kizob@gmail.com>
2021-05-26 16:09:11 +0200

    test

--- api/php/odp.php
+++ api/php/odp.php
@@ -10,16 +10,29 @@ $connect = new mysqli($servername, $username, $password, $dbname);
   $validation_error = '';
 
   
-  
-  $zapytanie = "SELECT a_temat FROM ankieta INNER JOIN ankieta WHERE mail.m_a_id=ankieta.a_id and mail='bozik.kizob@gmail.com'";
-  $result2 = mysqli_query($connect,$zapytanie);
-
-  if(mysqli_num_rows($result2)>0)
-  {
+  $test ="SELECT odpowiedz FROM mail INNER JOIN ankieta ON mail.m_a_id=ankieta.a_id where mail='bozik.kizob@gmail.com'";
+  $result = mysqli_query($connect,$test);
+  while($row1 = mysqli_fetch_array($result)){
+//   $id=$row1[]
+  if($row1['odpowiedz']==NULL){
+    $zapytanie = "SELECT a_temat FROM ankieta INNER JOIN mail ON ankieta.a_id=mail.m_a_id where mail='bozik.kizob@gmail.com'";
+    $result2 = mysqli_query($connect,$zapytanie);
     while($row = mysqli_fetch_array($result2)){
-        echo "Temat :".$row['a_temat']."<br>";}
+            if(mysqli_num_rows($result2)>0)
+            {
+                while($row = mysqli_fetch_array($result2)){
+                    echo "Temat :".$row['a_temat']."<br>";}
 
-  }
+            }
+            }
+        } }
+
+//   if(mysqli_num_rows($result2)>0)
+//   {
+//     while($row = mysqli_fetch_array($result2)){
+//         echo "Temat :".$row['a_temat']."<br>";}
+
+//   }
  
 
 ?>
