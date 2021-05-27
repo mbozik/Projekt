eb27a11474dc88612330807a550ac03c5b7bc849
mbozik <bozik.kizob@gmail.com>
2021-05-27 16:33:13 +0200

    test

--- api/profile.php
+++ api/profile.php
@@ -129,15 +129,10 @@
             <input id="key" type="text" class="form-control" placeholder="Klucz" name="key">
             
         </div>
-        <button type="submit" class="btn btn-dark" onclick="">Pobierz ankiete</button>
+        <button type="submit" class="btn btn-dark" onclick="" style="width:31%;">Pobierz ankiete</button>
         </form>
     
     </div>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
-        crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
-        crossorigin="anonymous"></script>
+
 </body>
 </html>
