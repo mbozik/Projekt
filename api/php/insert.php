a0105921b9d1c741ba10e00381931b397e9b66a6
mbozik <bozik.kizob@gmail.com>
2021-06-09 11:51:58 +0200

    test

--- api/php/insert.php
+++ api/php/insert.php
@@ -1,49 +1,7 @@
 <?php
-<<<<<<< HEAD
-include('connect.php');
-
-  session_start();
-   // $a=$tab[0];
-  $validation_error = '';
-
-  $title = $_POST['survey_title'];
-  $description = $_POST['survey_opis'];
-  $user = $_SESSION['name'];
-  $wartosc=$_POST['wartosc'];
-  
- 
-  $zapytanie = "SELECT a_temat FROM ankieta WHERE a_temat = '$title'";
-  $result2 = mysqli_query($connect,$zapytanie);
- 
-  if(mysqli_num_rows($result2) ==NULL){
-  $sql = "INSERT INTO ankieta(a_id, a_temat, a_opis, tworca) VALUES ('','$title','$description','$user')";
-  $result = $connect->query($sql);
-  $connect->close();
-
-
-  // $zap = "SELECT a_id FROM ankieta WHERE a_temat = '$title'";
-  // $result3 = mysqli_query($connect,$zap);
-  // if(mysqli_num_rows($result3) ==NULL){
-
-  // $sql = "INSERT INTO mail(mail_id, mail, odpowiedz, m_a_id) VALUES ('','$title','$description','$user')";
-  // $result = $connect->query($sql);
-  // $connect->close();
-
-  // header("Location: http://127.0.0.1/Projekt-1/api/question.php");}
-  header("Location: http://localhost/Projekt/api/question.php");}
-  else{
-   $validation_error = 'Podana ankieta istnieje';
-  header("refresh:4;url=http://localhost/Projekt/api/create.php");
-  //  header("refresh:4;url=http://127.0.0.1/Projekt-1/api/create.php");
-  
-   echo '<body style="background-color: #366d7e;"><p style="margin:0 auto; margin-top:20%; padding:5%; width:60%; text-align:center; background-color: #e7586287;font-size:25px;border:solid 2px black;">Ankieta '.$title.' istnieje już w bazie.<br> Za 4 sekund zostaniesz przeniesiony do strony z tworzeniem ankiety.</p>';
-  }
-?>
-=======
 /**
  * Plik wpisuje dane do tabeli ankieta  oraz przekierowuje do strony odpowiadającej za dodawanie pytań
  */
->>>>>>> 7046241c8c6747e1bfb263f982ae20c5f1e4553d
 
 include 'connect.php';
 
